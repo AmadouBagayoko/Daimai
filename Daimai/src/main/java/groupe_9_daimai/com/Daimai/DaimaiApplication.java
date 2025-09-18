6eb64bbf322c26a9a8ebbdd6a8112b28e92134ed
Amadou Bagayoko <abagayoko304@gmail.com>
2025-09-18 14:13:26 +0000

    m

---
+++ Daimai/src/main/java/groupe_9_daimai/com/Daimai/DaimaiApplication.java
@@ -0,0 +1,13 @@
+package groupe_9_daimai.com.Daimai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaimaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaimaiApplication.class, args);
+	}
+
+}
---
+++ Daimai/src/test/java/groupe_9_daimai/com/Daimai/DaimaiApplicationTests.java
@@ -0,0 +1,13 @@
+package groupe_9_daimai.com.Daimai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaimaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
